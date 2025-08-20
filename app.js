838be49e8952800b86036e62a8d12f71b228b3d0
codebyMurshid <114920559+codebyMurshid@users.noreply.github.com>
2025-08-19 23:53:04 -0500

    Added lines of codes

---
+++ app.js
@@ -0,0 +1,33 @@
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const categorySelect = document.getElementById("categorySelect");
+    const tasklist = document.getElementById("tasklist");
+
+    //Create elements
+    const li = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    const taskText = document.createElement("span");
+    taskText.classList.add("task-text");
+    taskText.textContent = taskInput.value;
+
+    const category = document.createElement("span");
+    category.classList.add("category", categorySelect.value);
+    category.textContent = categorySelect.value.charAt(0).toUpperCase() + categorySelect.value.slice(1);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.textContent = "X";
+    deleteBtn.onclick = () => li.remove();
+
+    //Append
+    li.appendChild(checkbox);
+    li.appendChild(taskText);
+    li.appendChild(category);
+    li.appendChild(deleteBtn);
+
+    tasklist.appendChild(li);
+}
+
+
