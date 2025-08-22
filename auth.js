8e19e3e73a1b7a0136d0dc2c29d8235566d3c52c
codebyMurshid <114920559+codebyMurshid@users.noreply.github.com>
2025-08-22 01:36:44 -0500

    Added Vite

--- auth.js
+++ auth.js
@@ -3,6 +3,8 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const credentialserror = document.querySelector(".credentialserror");
 
+const clientId = "923216260193-hhgkre5bbrso32kcson7hnhsg07j9j58.apps.googleusercontent.com";
+
 const useremail = ["test", "test2"];
 const userpass = ["123", "456"];
 
@@ -33,3 +35,24 @@ login.addEventListener("click", function () {
         });
     }
 });
+
+window.onload = function () {
+    google.accounts.id.initialize({
+        client_id: clientId,
+        callback: handleCredentialResponse
+    });
+
+    document.getElementById("google-login").addEventListener("click", () => {
+        google.accounts.id.prompt(); // show popup
+    });
+};
+
+function handleCredentialResponse(response) {
+    const payload = JSON.parse(atob(response.credential.split(".")[1]));
+
+    document.getElementById("userInfo").innerHTML = `
+    <p><strong>Welcome, ${payload.name}</strong></p>
+    <p>Email: ${payload.email}</p>
+    <img src="${payload.picture}" width="80"/>
+  `;
+}
